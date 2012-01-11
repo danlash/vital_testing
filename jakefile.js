8b5dd35ddfe2d07a38f60c6e84036a5240ba8b47
Dan Lash <danlash@gmail.com>
2012-01-11 11:50:18 -0500

    do not need

--- jakefile.js
+++ jakefile.js
@@ -13,34 +13,3 @@ task('specs', function(){
       complete();
     });  
 })
-
-
-var walk = function(dir, done) {
-  var results = [];
-
-  fs.readdir(dir, function(err, list) {
-    if (err) return done(err);
-
-    var pending = list.length;
-    if (!pending) return done(null, results);
-
-    list.forEach(function(file) {
-      file = dir + '/' + file;
-
-      fs.stat(file, function(err, stat) {
-
-        if (stat && stat.isDirectory()) {
-
-          walk(file, function(err, res) {
-            results = results.concat(res);
-            if (!--pending) done(null, results);
-          });
-
-        } else {
-          results.push(file);
-          if (!--pending) done(null, results);
-        }
-      });
-    });
-  });
-};               
