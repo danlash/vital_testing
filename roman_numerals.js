5e3a992f296257518f03311c21d6ffc955c4d2e3
Dan Lash <danlash@gmail.com>
2012-01-11 10:25:25 -0500

    refactor for two methods

--- roman_numerals.js
+++ roman_numerals.js
@@ -3,23 +3,33 @@ var should = require('should')
 
 function RomanNumeralCalculator(){
   this.calculate = function(number){
-    switch(number){
-      case 1:
-        return 'I';
-      case 'I':
-        return 1;
-      case 5:
-        return 'V';
-      case 10:
-        return 'X';
-      case 50:
-        return 'L';
-      case 100:
-        return 'C';
-      case 500:
-        return 'D';
-      case 1000:
-        return 'M';
+    if (typeof number === 'number')
+      return arabicToRoman(number)
+    else
+      return romanToArabic(number)
+
+    function arabicToRoman(number){
+      switch(number){
+        case 1: return 'I';
+        case 5: return 'V';
+        case 10: return 'X';
+        case 50: return 'L';
+        case 100: return 'C';
+        case 500: return 'D';
+        case 1000: return 'M';
+      }
+    }
+
+    function romanToArabic(number){
+      switch(number){
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+      } 
     }
   }
 }
@@ -38,10 +48,10 @@ describe('number to roman conversion', function(){
         it(roman + ' should be ' + arabic, function(){
           calc.calculate(arabic).should.equal(roman)
         })
-    })
 
-    it('I should be 1', function(){
-      calc.calculate('I').should.equal(1);
+        it(arabic + ' should be ' + roman, function(){
+          calc.calculate(roman).should.equal(arabic)
+        })
     })
 
   })
